bb0bdf8e2f51b16fabf05fab20fb973ad0dbf8d9
sulvto <lpetrel@outlook.com>
2016-06-11 00:52:57 +0800

    algorithm.js

--- algorithm.js
+++ algorithm.js
@@ -81,19 +81,19 @@ function drawPath() {
     var steps = mapElement.querySelectorAll('span.step');
     console.log(steps);
     if (steps.length > 0) {
-
-
         steps.forEach(function (step) {
             step.className = '';
         });
+    }
 
-        var from = {x: from.x, y: from.y};
-        var to = {x: to.x, y: to.y};
+    var from = {x: from.x, y: from.y};
+    var to = {x: to.x, y: to.y};
 
-        var path = aStarSearch(from, to, map);
-
+    var path = aStarSearch(from, to, map);
+    if (path) {
         drawSteps(path);
     }
+
 }
 
 function drawSteps(path) {
@@ -106,6 +106,190 @@ function drawSteps(path) {
     }
 }
 
+function aStarSearch(from, to, map) {
+    var ORTHOGONAL = 10;
+    var DIAGONAL = 14;
+
+    var height = map.length;
+    var width = map[0].length;
+
+    var openList = [];
+    var closedList = [];
+
+    function isOpen(node) {
+        for (var i = 0; i < openList.length; i++) {
+            var item = openList[i];
+            if (item.x == node.x && item.y == node.y) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function isClosed(node) {
+        return closedList[node.x] && closedList[node.x][node.y];
+    }
+
+    function pushClosedList(node) {
+        if (!closedList[node.x]) {
+            closedList[node.x] = [];
+        }
+        closedList[node.x][node.y] = node;
+    }
+
+    //  (x-1,y-1) (x,y-1) (x+1,y-1)
+    //   (x-1,y)   (x,y)   (x+1,y)
+    //  (x-1,y+1) (x,y+1) (x+1,y+1)
+
+    //  UPLEFT     UP     UPRIGHT
+    //  LEFT     parent     RIGHT
+    //  DOWNLEFT  DOWN  DOWNRIGHT
+
+    // Math.abs(x) 函数返回指定数字“x“ 的绝对值
+
+    // F = G + H
+
+    // * G = 从起点A，沿着产生的路径，移动到网格上指定方格的移动耗费。
+    // * H = 从网格上那个方格移动到终点B的预估移动耗费
+
+    function getSurroundNode(parent) {
+        var surroundList = [];
+        var up = null;
+        var down = null;
+        var left = null;
+        var right = null;
+
+        if (!parent.g) {
+            parent.g = 0;
+        }
+        // LEFT
+        if (parent.x > 0) {
+            var node = map[parent.x - 1][parent.y];
+            if (node.walkable && !isClosed(node)) {
+                node.parent = parent;
+                left = node;
+                node.g = parent.g + ORTHOGONAL;
+                var h = ORTHOGONAL * Math.abs(node.x - to.x) + Math.abs(node.y - to.y);
+                node.f = h + node.g;
+                surroundList.push(node);
+            }
+        }
+
+        // RIGHT
+        if (parent.x <= width) {
+            var node = map[parent.x + 1][parent.y];
+            if (node.walkable && !isClosed(node)) {
+                node.parent = parent;
+                right = node;
+                node.g = parent.g + ORTHOGONAL;
+                var h = ORTHOGONAL * Math.abs(node.x - to.x) + Math.abs(node.y - to.y);
+                node.f = h + node.g;
+                surroundList.push(node);
+            }
+        }
+
+        // UP
+        if (parent.y > 0) {
+            var node = map[parent.x][parent.y - 1];
+            if (node.walkable && !isClosed(node)) {
+                node.parent = parent;
+                up = node;
+                node.g = parent.g + ORTHOGONAL;
+                var h = ORTHOGONAL * Math.abs(node.x - to.x) + Math.abs(node.y - to.y);
+                node.f = h + node.g;
+                surroundList.push(node);
+            }
+        }
+        // DOWN
+        if (parent.y <= height) {
+            var node = map[parent.x][parent.y + 1];
+            if (node.walkable && !isClosed(node)) {
+                node.parent = parent;
+                down = node;
+                node.g = parent.g + ORTHOGONAL;
+                var h = ORTHOGONAL * Math.abs(node.x - to.x) + Math.abs(node.y - to.y);
+                node.f = h + node.g;
+                surroundList.push(node);
+            }
+        }
+        // UPLEFT
+        if ((up || left ) && parent.x > 0 && parent.y > 0) {
+            var node = map[parent.x - 1][parent.y - 1];
+            if (node.walkable && !isClosed(node)) {
+                node.parent = parent;
+                node.g = parent.g + DIAGONAL;
+                var h = ORTHOGONAL * Math.abs(node.x - to.x) + Math.abs(node.y - to.y);
+                node.f = h + node.g;
+                surroundList.push(node);
+            }
+        }
+
+        // DOWNLEFT
+        if ((down || left) && parent.x > 0 && parent.y <= height) {
+            var node = map[parent.x - 1][parent.y + 1];
+            if (node.walkable && !isClosed(node)) {
+                node.parent = parent;
+                node.g = parent.g + DIAGONAL;
+                var h = ORTHOGONAL * Math.abs(node.x - to.x) + Math.abs(node.y - to.y);
+                node.f = h + node.g;
+                surroundList.push(node);
+            }
+        }
+
+        // UPRIGHT
+        if ((up || right) && parent.x <= width && parent.y > 0) {
+            var node = map[parent.x + 1][parent.y - 1];
+            if (node.walkable && !isClosed(node)) {
+                node.parent = parent;
+                node.g = parent.g + DIAGONAL;
+                var h = ORTHOGONAL * Math.abs(node.x - to.x) + Math.abs(node.y - to.y);
+                node.f = h + node.g;
+                surroundList.push(node);
+            }
+        }
+
+        // DOWNRIGHT
+        if ((down || right) && parent.x <= width && parent.y <= height) {
+            var node = map[parent.x + 1][parent.y + 1];
+            if (node.walkable && !isClosed(node)) {
+                node.parent = parent;
+                node.g = parent.g + DIAGONAL;
+                node.h = ORTHOGONAL * Math.abs(node.x - to.x) + Math.abs(node.y - to.y);
+                node.f = node.h + node.g;
+                surroundList.push(node);
+            }
+        }
+
+        return surroundList;
+    }
+
+    openList.push(from);
+
+    for (; openList.length;) {
+        var current = openList.sort(function (a, b) {
+            return b.f - a.f;
+        }).pop();
+        var surroundNode = getSurroundNode(current);
+        for (var i = 0; i < surroundNode.length; i++) {
+            var item = surroundNode[i];
+            if (item.x === to.x && item.y === to.y) {
+                var path = [];
+                var parent = item.parent;
+                while (parent) {
+                    path.push(parent);
+                    parent = parent.parent;
+                }
+                return path.reverse();
+            }
+            if (!isOpen(item)) {
+                openList.push(item);
+            }
+        }
+
+        pushClosedList(current);
+    }
+    return null;
+}
 
 initMap();
 addEventListener();
